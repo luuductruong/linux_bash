caafa9c3354ffdc585310bb885f964997a737c4a
luuductruong <luuductruong96@gmail.com>
2022-07-23 10:46:14 +0700

    change print: add mo decription to ip

--- ip_local_hcm.sh
+++ ip_local_hcm.sh
@@ -5,7 +5,7 @@ Kz1=192.168.1.101/32
 Kz2=192.168.1.102/32
 Km4=192.168.1.103/32
 
-echo -n "Status $Kz1:   "
+echo -n "Status Kazoo1: $Kz1:   "
 if ip route add $Kz1 via 192.168.0.1 dev $Internet
 then
   echo "kazoo4-01: ok"
@@ -13,7 +13,7 @@ else
   STATUS=false
 fi
 
-echo -n "Status $Kz2:   "
+echo -n "Status Kazoo2: $Kz2:   "
 if ip route add $Kz2 via 192.168.0.1 dev $Internet
 then
   echo "kazoo4-02: ok"
@@ -21,7 +21,7 @@ else
   STATUS=false
 fi
 
-echo -n "Status $Km4:   "
+echo -n "Status Kamailio4: $Km4:   "
 if ip route add $Km4 via 192.168.0.1 dev $Internet
 then 
   echo "kamailio:  ok"
@@ -31,9 +31,9 @@ fi
 
 if $STATUS
 then 
-  echo "done het roi ne hehehe"
+  echo "Done het roi ne hehehe"
 else
-  echo "sai roi nha :'("
+  echo "Sai roi nha :'("
 fi
 unset STATUS Kz1 Kz2 Km4 Internet
 
