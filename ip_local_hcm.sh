64d681f2ce04844c6ada5f938c5ff52a5ba098b9
luuductruong <luuductruong96@gmail.com>
2022-07-23 10:40:23 +0700

    add ip route via local tun0

---
+++ ip_local_hcm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+STATUS=true
+Internet=tun0
+Kz1=192.168.1.101/32
+Kz2=192.168.1.102/32
+Km4=192.168.1.103/32
+
+echo -n "Status $Kz1:   "
+if ip route add $Kz1 via 192.168.0.1 dev $Internet
+then
+  echo "kazoo4-01: ok"
+else
+  STATUS=false
+fi
+
+echo -n "Status $Kz2:   "
+if ip route add $Kz2 via 192.168.0.1 dev $Internet
+then
+  echo "kazoo4-02: ok"
+else
+  STATUS=false
+fi
+
+echo -n "Status $Km4:   "
+if ip route add $Km4 via 192.168.0.1 dev $Internet
+then 
+  echo "kamailio:  ok"
+else
+  STATUS=false
+fi
+
+if $STATUS
+then 
+  echo "done het roi ne hehehe"
+else
+  echo "sai roi nha :'("
+fi
+unset STATUS Kz1 Kz2 Km4 Internet
+
+: '
+ar='test comment'
+echo $ar
+echo "mmuahah"
+'
